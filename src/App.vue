b6b4d61118348cafd02286f0145e99d59c0222c5
Ivar <63834@gymnasium.ax>
2025-11-03 12:44:44 +0200

    Lagt till nytt projekt för sten sax påse

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import KnappRad from './components/KnappRad.vue'
+import NollStall from './components/NollStall.vue'
+import PoangRad from './components/PoangRad.vue'
+import ResultatRad from './components/ResultatRad.vue'
+</script>
+
+<template>
+  <KnappRad />
+  <ResultatRad />
+  <PoangRad />
+  <NollStall />
+</template>
+
+<style scoped></style>
---
+++ src/components/NollStall.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>NollStall</template>
+
+<style scoped></style>
